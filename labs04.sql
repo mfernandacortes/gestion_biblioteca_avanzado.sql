2ea256ac83c3c0862483eb1add9892691380563f
Maria Fernanda Cortes <mfercortes@gmail.com>
2025-03-06 10:57:13 -0300

    Subir archivo SQL

---
+++ labs04.sql
@@ -0,0 +1,212 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-08-2019 a las 18:26:07
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.1.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `labs04`
+--
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertar_autor` (`NomyAp` VARCHAR(200))  INSERT INTO `autores`(`IdAutor`, `ApellidoyNomAutor`) VALUES ('', NomyAp)$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autores`
+--
+
+CREATE TABLE `autores` (
+  `IdAutor` int(11) NOT NULL,
+  `ApellidoyNomAutor` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `autores`
+--
+
+INSERT INTO `autores` (`IdAutor`, `ApellidoyNomAutor`) VALUES
+(1, 'Cortazar Julio'),
+(2, 'Borges Jorge Luis'),
+(3, 'García Marquez Gabriel');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `generos`
+--
+
+CREATE TABLE `generos` (
+  `IdGenero` int(11) NOT NULL,
+  `DetalleGenero` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`IdGenero`, `DetalleGenero`) VALUES
+(1, 'Poesía'),
+(2, 'Novelas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `libros`
+--
+
+CREATE TABLE `libros` (
+  `IdLibro` int(11) NOT NULL,
+  `AutorId` int(11) NOT NULL,
+  `GeneroId` int(11) NOT NULL,
+  `Titulo` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `Disponible` tinyint(1) NOT NULL,
+  `fecha_prestamo` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `libros`
+--
+
+INSERT INTO `libros` (`IdLibro`, `AutorId`, `GeneroId`, `Titulo`, `Disponible`, `fecha_prestamo`) VALUES
+(1, 1, 2, 'Rayuela', 1, NULL),
+(2, 3, 2, 'Cien años de soledad', 0, NULL),
+(3, 2, 1, 'El hacedor', 0, '2019-08-14');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `IdPrestamo` int(11) NOT NULL,
+  `SocioDni` int(11) NOT NULL,
+  `LibroId` int(11) NOT NULL,
+  `FechaPrestamo` date NOT NULL,
+  `FechaDevolucion` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `prestamos`
+--
+
+INSERT INTO `prestamos` (`IdPrestamo`, `SocioDni`, `LibroId`, `FechaPrestamo`, `FechaDevolucion`) VALUES
+(1, 11111111, 2, '2019-08-10', NULL),
+(2, 11111111, 3, '2019-08-24', NULL),
+(12, 11111111, 3, '2019-08-14', NULL);
+
+--
+-- Disparadores `prestamos`
+--
+DELIMITER $$
+CREATE TRIGGER `modif_libro` AFTER INSERT ON `prestamos` FOR EACH ROW update libros set Disponible = 0, fecha_prestamo=new.FechaPrestamo where IdLibro=new.LibroId
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `socios`
+--
+
+CREATE TABLE `socios` (
+  `DniSocio` int(11) NOT NULL,
+  `NomyApSocio` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `Telefono` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `socios`
+--
+
+INSERT INTO `socios` (`DniSocio`, `NomyApSocio`, `Telefono`) VALUES
+(11111111, 'Arana Facundo', 34156565),
+(54454556, 'Pitt Brad', 341589889);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `autores`
+--
+ALTER TABLE `autores`
+  ADD PRIMARY KEY (`IdAutor`);
+
+--
+-- Indices de la tabla `generos`
+--
+ALTER TABLE `generos`
+  ADD PRIMARY KEY (`IdGenero`);
+
+--
+-- Indices de la tabla `libros`
+--
+ALTER TABLE `libros`
+  ADD PRIMARY KEY (`IdLibro`);
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`IdPrestamo`);
+
+--
+-- Indices de la tabla `socios`
+--
+ALTER TABLE `socios`
+  ADD PRIMARY KEY (`DniSocio`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `autores`
+--
+ALTER TABLE `autores`
+  MODIFY `IdAutor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `generos`
+--
+ALTER TABLE `generos`
+  MODIFY `IdGenero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `libros`
+--
+ALTER TABLE `libros`
+  MODIFY `IdLibro` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `IdPrestamo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
